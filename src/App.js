fcf9a9482e8d74b6485af27c2034d40aa2a12c6c
Salvation27 <nikolayzalipykin@gmail.com>
2022-01-21 09:48:00 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Paper, Divider, Button, List, Tabs, Tab } from '@mui/material';
+import { AddField } from './components/AddField';
+import { Item } from './components/Item';
+
+function App() {
+  return (
+    <div className="App">
+      <Paper className="wrapper">
+        <Paper className="header" elevation={0}>
+          <h4>Список задач</h4>
+        </Paper>
+        <AddField />
+        <Divider />
+        <Tabs value={0}>
+          <Tab label="Все" />
+          <Tab label="Активные" />
+          <Tab label="Завершённые" />
+        </Tabs>
+        <Divider />
+        <List>
+          <Item text="Задача №1" />
+        </List>
+        <Divider />
+        <div className="check-buttons">
+          <Button>Отметить всё</Button>
+          <Button>Очистить</Button>
+        </div>
+      </Paper>
+    </div>
+  );
+}
+
+export default App;
